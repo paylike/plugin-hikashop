56089a2e7634f1d01b3308315e844e8249210d7b
LucianDex <remindfwd@gmail.com>
2022-02-01 15:14:48 +0200

    Added custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+/**
+ * Parent commands
+ */
+
+/**
+ * Go to specified Url
+ * Enhanced with auth for HTTP protected websites
+ */
+ Cypress.Commands.add('goToPage', (pageUrl) => {
+    if (Cypress.env('ENV_HTTP_AUTH_ENABLED')) {
+        cy.visit(pageUrl, {
+            auth: {
+                username: Cypress.env('ENV_HTTP_USER'),
+                password: Cypress.env('ENV_HTTP_PASS'),
+            },
+        });
+    } else {
+        cy.visit(pageUrl);
+    }
+});
