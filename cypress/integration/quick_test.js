60342ea649650880127cdfd9d150f5ee682a666d
LucianDex <remindfwd@gmail.com>
2022-02-01 16:57:15 +0200

    Updated & code rearrange quic_test

--- cypress/integration/quick_test.js
+++ cypress/integration/quick_test.js
@@ -2,38 +2,46 @@
 
 'use strict';
 
-import { helper } from "./helper.js";
+import { PaylikeTestHelper } from './hikashop_helper.js';
 
 describe('paylike plugin quick test', () => {
 
-    /** Get ENV variables to use forward. */
-    const AdminUrl = Cypress.env("ENV_ADMIN_URL");
-    const HttpUsername = Cypress.env('ENV_HTTP_USER');
-    const HttpPassword = Cypress.env('ENV_HTTP_PASS');
-
-    const AdminUsername = Cypress.env('ENV_ADMIN');
+    /** Admin & frontend user credentials. */
+    const StoreUrl = Cypress.env('ENV_STORE_URL');
+    const AdminUrl = Cypress.env('ENV_ADMIN_URL');
+    const AdminUsername = Cypress.env('ENV_ADMIN_USER');
     const AdminPassword = Cypress.env('ENV_ADMIN_PASS');
+    const StoreUsername = Cypress.env('ENV_CLIENT_USER');
+    const StorePassword = Cypress.env('ENV_CLIENT_PASS');
 
     const RemoteVersionLogUrl = Cypress.env('REMOTE_LOG_URL');
     const CurrencyToChangeWith = Cypress.env('ENV_CURRENCY_TO_CHANGE_WITH');
+    const CaptureMode = Cypress.env('ENV_CAPTURE_MODE');
+    const CheckoutMode = Cypress.env('ENV_CHECKOUT_MODE');
+
+    /**
+     * Constants used to make or skip some tests.
+     */
+    const NeedToAdminLogin = true === Cypress.env('ENV_STOP_EMAIL') ||
+                             true === Cypress.env('ENV_LOG_VERSION') ||
+                             true === Cypress.env('ENV_SETTINGS_CHECK');
 
     /** Construct some variables to be used bellow. */
     const PaylikeName = 'paylike';
     const HikashopAdminUrl = (AdminUrl + '/index.php?option=com_hikashop');
-    // const ManageExtensionsAdminUri = 'index.php?option=com_installer&view=manage';
     const ManageExtensionsAdminUrl = (AdminUrl + '/index.php?option=com_installer&view=manage');
     const ManageEmailSettingUrl = (AdminUrl + '/index.php?option=com_hikashop&ctrl=email');
+    const ManagePaylikeSettingUrl = (AdminUrl + '/index.php?option=com_hikashop&ctrl=plugins&plugin_type=payment');
+    const OrdersPageAdminUrl = (AdminUrl + '/index.php?option=com_hikashop&ctrl=order&order_type=sale');
+
 
     /**
-     * Go to backend site admin
+     * Go to backend site admin if necessary
      */
     before(() => {
-        cy.visit(AdminUrl, {
-            auth: {
-                username: HttpUsername,
-                password: HttpPassword,
-              },
-        });
+        if (NeedToAdminLogin) {
+            cy.goToPage(AdminUrl);
+        }
     });
 
     /**
@@ -45,105 +53,192 @@ describe('paylike plugin quick test', () => {
     });
 
     /**
-     * Login into admin.
+     * Login into admin if necessary.
      */
-    it("manage Paylike module from admin", () => {
-        /** Select username & password inputs, then press enter. */
-        cy.get("input[name=username]").type(`${AdminUsername}`);
-        cy.get("input[name=passwd]").type(`${AdminPassword}{enter}`);
-    });
+    if (NeedToAdminLogin) {
+        it('login into admin backend', () => {
+            /** Select username & password inputs, then press enter. */
+            cy.get('input[name=username]').type(`${AdminUsername}`);
+            cy.get('input[name=passwd]').type(`${AdminPassword}{enter}`);
+        });
+    }
 
     /**
      * Get Hikashop & Paylike versions and send log data.
      */
-    it("log hikashop & paylike versions remotely", () => {
+    if (Cypress.env('ENV_LOG_VERSION') ?? false) {
+        it('log hikashop & paylike versions remotely', () => {
+            /** Go to hikashop section from admin. */
+            cy.goToPage(HikashopAdminUrl);
 
-        /** Go to hikashop section from admin. */
-        cy.visit(HikashopAdminUrl, {
-            auth: {
-                username: HttpUsername,
-                password: HttpPassword,
-            },
-        });
+            cy.get('.hikashop_footer').then(($hikashopFooter) => {
+                var footerText = $hikashopFooter.text();
+                var hikashopVersion = footerText.replace(/[^0-9.]/g, '');
+                cy.wrap(hikashopVersion).as('hikashopVersion');
+            });
 
-        cy.get('.hikashop_footer').then(($hikashopFooter) => {
-            var footerText = $hikashopFooter.text();
-            var hikashopVersion = footerText.replace(/[^0-9.]/g, '');
-            cy.wrap(hikashopVersion).as('hikashopVersion');
-        });
+            /** Go to system settings admin page. */
+            cy.goToPage(ManageExtensionsAdminUrl);
 
-        /** Go to system settings admin page. */
-        cy.visit(ManageExtensionsAdminUrl, {
-            auth: {
-                username: HttpUsername,
-                password: HttpPassword,
-            },
-        });
+            /** Set position relative for toolbar. */
+            PaylikeTestHelper.setPositionRelativeOn('#subhead-container');
 
-        /** Change toolbar position to relative (to not cover search input during cypress test). */
-        cy.get('#subhead-container').then(($toolbarNav) => {
-            $toolbarNav.attr('style', 'position:relative;');
-        });
+            /** Select search input and type "paylike", then press enter. */
+            cy.get('input[name="filter[search]"]').type(`${PaylikeName}{enter}`);
 
-        /** Select search input and type "paylike", then press enter. */
-        cy.get('input[name="filter[search]"]').type(`${PaylikeName}{enter}`);
+            /** Select row from plugins table to get plugin version from there. */
+            cy.get('tbody tr').each(($element, index, $list) => {
+                var rowText = $element.text();
+                if (rowText.includes('hikashoppayment')) {
+                    cy.get('tr td:nth-child(6)').eq(index).then($pluginVersion => {
+                        var paylikePaymentVersion = $pluginVersion.text();
+                        /** Make global variable to be accessible bellow. */
+                        cy.wrap(paylikePaymentVersion).as('paylikePaymentVersion');
+                    });
+                }
+            });
 
-        /** Select row from plugins table to get plugin version from there. */
-        cy.get("tbody tr").each(($element, index, $list) => {
-            /** Get element text. */
-            var rowText = $element.text();
+            /** Get global variables and make log data request to remote url. */
+            cy.get('@hikashopVersion').then(hikashopFooterVersion => {
+                cy.get('@paylikePaymentVersion').then(paylikeModuleVersion => {
 
-            /** Check if all row text contain specific text. */
-            if(rowText.includes("hikashoppayment")) {
-                /** Select specific column from matched row, then get plugin version. */
-                cy.get('tr td:nth-child(6)').eq(index).then($pluginVersion => {
-                    var paylikePaymentVersion = $pluginVersion.text();
-                    /** Make global variable to be accessible bellow. */
-                    cy.wrap(paylikePaymentVersion).as('paylikePaymentVersion');
-                });
-            }
-        });
-
-        /** Get global variables and make log data request to remote url. */
-        cy.get('@hikashopVersion').then(hikashopFooterVersion => {
-            cy.get('@paylikePaymentVersion').then(paylikeModuleVersion => {
-
-                cy.request('GET', RemoteVersionLogUrl, {
-                    key: hikashopFooterVersion,
-                    tag: 'hikashop',
-                    view: 'html',
-                    ecommerce: hikashopFooterVersion,
-                    plugin: paylikeModuleVersion
-                }).then((resp) => {
-                    expect(resp.status).to.eq(200);
+                    cy.request('GET', RemoteVersionLogUrl, {
+                        key: hikashopFooterVersion,
+                        tag: 'hikashop',
+                        view: 'html',
+                        ecommerce: hikashopFooterVersion,
+                        plugin: paylikeModuleVersion
+                    }).then((resp) => {
+                        expect(resp.status).to.eq(200);
+                    });
                 });
             });
         });
-
-    });
+    }
 
     /**
-     * Modify hikashop settings
+     * Modify Hikashop email settings (disable notifications)
      */
-    it("modify hikashop settings", () => {
-        /** Go to system settings admin page. */
-        cy.visit(ManageEmailSettingUrl, {
-            auth: {
-                username: HttpUsername,
-                password: HttpPassword,
-            },
+    if (Cypress.env('ENV_STOP_EMAIL') ?? false) {
+        it('modify Hikashop settings for email notifications', () => {
+            /** Go to hikashop email settings page. */
+            cy.goToPage(ManageEmailSettingUrl);
+
+            /** Set position relative for toolbar. */
+            PaylikeTestHelper.setPositionRelativeOn('#subhead-container');
+
+            /** Disable admin email notifications. */
+            cy.get('span[id*="config_value-order_"]').each(($element) => {
+                console.log($element.attr('id'))
+                if (
+                    'config_value-order_notification.published' == $element.attr('id') ||
+                    'config_value-order_admin_notification.published' == $element.attr('id')
+                ) {
+                    var $spanLink = $element.children('a')
+                    if ($spanLink.hasClass('icon-publish')) {
+                        $spanLink.trigger('click');
+                    }
+                }
+            });
         });
+    }
 
-        /** Wait for page elements to loading. */
-        cy.wait(1000);
+    /**
+     * Modify Paylike settings
+     */
+    if (Cypress.env('ENV_SETTINGS_CHECK') ?? false) {
+        it('modify Paylike settings for capture mode', () => {
+            /** Go to plugins page, and select Paylike. */
+            cy.goToPage(ManagePaylikeSettingUrl);
 
-        /** Disable email notifications. */
-        // cy.get('#config_value-order_notification.published').click();
-        // cy.get('#config_value-order_admin_notification.published').click();
-        // cy.get('.hk_center #config_value-order_admin_notification.published .icon-unpublish');
+            /** Set position relative for toolbar. */
+            PaylikeTestHelper.setPositionRelativeOn('#subhead-container');
+
+            /** Select search input and type "paylike", then press enter. */
+            cy.get('input[name=search]').type(`${PaylikeName}{enter}`);
+
+            /** Select link to Paylike settings. */
+            cy.get('a[href*="' + `=edit&name=${PaylikeName}` + '"]').click();
+
+            /** Set position relative for toolbar. */
+            PaylikeTestHelper.setPositionRelativeOn('#subhead-container');
+
+            /** Change capture mode. */
+            cy.get('#datapaymentpayment_paramsinstant_mode').select(CaptureMode);
+            cy.get('#toolbar-save').click();
+        });
+    }
+
+    /**
+     * Make an instant payment
+     */
+    // it('makes an instant payment with Paylike', () => {
+    //     /** Go to store frontend. */
+    //     cy.goToPage(StoreUrl);
+
+    //     /** Client frontend login. */
+    //     cy.get('input[name=username]').type(`${StoreUsername}`);
+    //     cy.get('input[name=password]').type(`${StorePassword}{enter}`);
+
+    //     /** Change currency & wait for products price to finish update. */
+    //     cy.get('#hikashopcurrency option').each(($option) => {
+    //         if ($option.text().includes(CurrencyToChangeWith)) {
+    //             cy.get('#hikashopcurrency').select($option.val());
+    //         }
+    //     });
+    //     cy.wait(2000);
+
+    //     /** Add to cart random product. */
+    //     var randomInt = PaylikeTestHelper.getRandomInt(/*max*/ 6);
+    //     cy.get('.hikabtn.hikacart').eq(randomInt).click();
+
+    //     /** Wait for 'added to cart' notification to disappear */
+    //     cy.wait(3000);
+    //     cy.get('.notifyjs-metro-base.notifyjs-metro-info').should('not.exist');
+
+    //     /** Proceed to checkout. */
+    //     cy.get('.hikashop_cart_proceed_to_checkout').click();
+
+    // //// TODO calculate amount
+    // //// TODO calculate amount
+    // //// TODO calculate amount
+
+    //     /** Choose Paylike. */
+    //     cy.get(`input[id*=${PaylikeName}]`).click();
+
+    //     /** Go to checkout next step. */
+    //     cy.get('#hikabtn_checkout_next').click();
+
+    //     /** Check if order was placed. */
+    //     cy.get('#paylike_paying').should('be.visible');
+
+    //     /**
+    //      * Fill in Paylike popup.
+    //      */
+    //     PaylikeTestHelper.fillAndSubmitPaylikePopup();
+
+    //     /** Check if order was paid. */
+    //     cy.get('.hikashop_paylike_end #paylike_paid').should('be.visible');
+
+    // });
 
 
-    });
+    // it('process an order from admin panel', () => {
+    //     /** Go to admin orders page. */
+    //     cy.gotoPage(OrdersPageAdminUrl);
 
+    //     cy.get('.hikashop_order_number_value a').click();
+
+    //     // cy.get('.hikashop_order_ordernumber').click();
+    //     if ('Delayed' == CaptureMode) {
+
+    //         //capture
+    //         PaylikeTestHelper.changeOrderStatus('confirmed');
+
+    //     } else {
+    //         // refund
+    //         PaylikeTestHelper.changeOrderStatus('refunded');
+    //     }
+    // });
 
 }); // describe
